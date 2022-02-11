74140aa8e302054420a8706f5e10c283c61976f3
DelroyGayle <delroygayle01@gmail.com>
2022-02-11 12:29:12 +0000

    Update members.js
    Removed comment

--- members.js
+++ members.js
@@ -1,11 +1,3 @@
-/*
-first name ,last name ,job title         ,department  ,manager  ,location  ,first aider ,user slack ,timezone
-John       ,Doe       ,Software Engineer ,Engineering ,null     ,Glasgow   ,No          ,johndoe    ,America/Los_Angeles
-Jane       ,Doe       ,Software Engineer ,Engineering ,John Doe ,Leeds     ,No          ,janedoe    ,America/Los_Angeles
-Bob        ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Cape Town ,No          ,bobdoe     ,America/Los_Angeles
-Sally      ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Rome      ,No          ,sallydoe   ,America/Los_Angeles
-*/
-
 const members = [{
                   firstName: "John", lastName: "Doe", jobTitle: "Software Engineer", department: "Engineering", 
                   manager: "", location: "Glasgow", firstAider: false, userSlack: "johndoe", 
